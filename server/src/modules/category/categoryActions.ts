cf12e4a1358ae7b4f7ba9dd66b149bab11718c7f
alexandravelisar <anda_alexandra91@yahoo.com>
2026-09-02 13:30:32 +0200

    Add validation for categories and programs

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -61,6 +61,7 @@ const add: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
 const destroy: RequestHandler = async (req, res, next) => {
   try {
     const categoryId = Number(req.params.id);
@@ -73,6 +74,35 @@ const destroy: RequestHandler = async (req, res, next) => {
   }
 };
 
+const validate: RequestHandler = (req, res, next) => {
+  type ValidationError = {
+    field: string;
+    message: string;
+  };
+
+  const errors: ValidationError[] = [];
+
+  const { name } = req.body;
+
+  if (name == null) {
+    errors.push({
+      field: "name",
+      message: "The field is required",
+    });
+  } else if (name.length > 255) {
+    errors.push({
+      field: "name",
+      message: "Should contain less than 255 characters",
+    });
+  }
+
+  if (errors.length === 0) {
+    next();
+  } else {
+    res.status(400).json({ validationErrors: errors });
+  }
+};
+
 // Export them to import them somewhere else
 
-export default { browse, read, edit, add, destroy };
+export default { browse, read, edit, add, destroy, validate };
--- server/src/modules/program/programActions.ts
+++ server/src/modules/program/programActions.ts
@@ -77,6 +77,70 @@ const destroy: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
+const validate: RequestHandler = (req, res, next) => {
+  type ValidationError = {
+    field: string;
+    message: string;
+  };
+
+  const errors: ValidationError[] = [];
+
+  const { title, synopsis, poster, country, year, category_id } = req.body;
+
+  if (title == null) {
+    errors.push({
+      field: "title",
+      message: "The field is required",
+    });
+  } else if (title.length > 255) {
+    errors.push({
+      field: "title",
+      message: "Should contain less than 255 characters",
+    });
+  }
+
+  if (synopsis == null) {
+    errors.push({
+      field: "synopsis",
+      message: "The field is required",
+    });
+  }
+
+  if (poster == null) {
+    errors.push({
+      field: "poster",
+      message: "The field is required",
+    });
+  }
+
+  if (country == null) {
+    errors.push({
+      field: "country",
+      message: "The field is required",
+    });
+  }
+
+  if (year == null) {
+    errors.push({
+      field: "year",
+      message: "The field is required",
+    });
+  }
+
+  if (category_id == null) {
+    errors.push({
+      field: "category_id",
+      message: "The field is required",
+    });
+  }
+
+  if (errors.length === 0) {
+    next();
+  } else {
+    res.status(400).json({ validationErrors: errors });
+  }
+};
 // Export it to import it somewhere else
 
-export default { browse, read, edit, add, destroy };
+export default { browse, read, edit, add, destroy, validate };
