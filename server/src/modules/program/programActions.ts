7907cd458c1890c6cec67ac88da1640282dce416
alexandravelisar <anda_alexandra91@yahoo.com>
2026-09-02 10:46:17 +0200

    Add program read

--- server/src/modules/program/programActions.ts
+++ server/src/modules/program/programActions.ts
@@ -1,27 +1,5 @@
 import programRepository from "./programRepository";
 
-const programs = [
-  {
-    id: 1,
-    title: "The Good Place",
-    synopsis:
-      "À sa mort, Eleanor Shellstrop est envoyée au Bon Endroit, un paradis fantaisiste réservé aux individus exceptionnellement bienveillants. Or Eleanor n'est pas exactement une « bonne personne » et comprend vite qu'il y a eu erreur sur la personne. Avec l'aide de Chidi, sa prétendue âme sœur dans l'au-delà, la jeune femme est bien décidée à se redécouvrir.",
-    poster:
-      "https://img.betaseries.com/JwRqyGD3f9KvO_OlfIXHZUA3Ypw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2F94857341d71c795c69b9e5b23c4bf3e7.jpg",
-    country: "USA",
-    year: 2016,
-  },
-  {
-    id: 2,
-    title: "Dark",
-    synopsis:
-      "Quatre familles affolées par la disparition d'un enfant cherchent des réponses et tombent sur un mystère impliquant trois générations qui finit de les déstabiliser.",
-    poster:
-      "https://img.betaseries.com/zDxfeFudy3HWjxa6J8QIED9iaVw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2Fc47135385da176a87d0dd9177c5f6a41.jpg",
-    country: "Allemagne",
-    year: 2017,
-  },
-];
 // Declare the action
 import type { RequestHandler } from "express";
 
@@ -31,15 +9,19 @@ const browse: RequestHandler = async (req, res) => {
   res.json(programsFromDB);
 };
 
-const read: RequestHandler = (req, res) => {
-  const parsedId = Number(req.params.id);
+const read: RequestHandler = async (req, res, next) => {
+  try {
+    const programId = Number(req.params.id);
 
-  const program = programs.find((p) => p.id === parsedId);
+    const program = await programRepository.read(programId);
 
-  if (program != null) {
-    res.json(program);
-  } else {
-    res.sendStatus(404);
+    if (program == null) {
+      res.sendStatus(404);
+    } else {
+      res.json(program);
+    }
+  } catch (err) {
+    next(err);
   }
 };
 
--- server/src/modules/program/programRepository.ts
+++ server/src/modules/program/programRepository.ts
@@ -17,6 +17,14 @@ class ProgramRepository {
 
     return rows as Program[];
   }
+  async read(id: number) {
+    const [rows] = await databaseClient.query<Rows>(
+      "select * from program where id = ?",
+      [id],
+    );
+
+    return rows[0] as Program;
+  }
 }
 
 export default new ProgramRepository();
